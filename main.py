d5b1abc49b0dc77c0cda975d1c10a22f99b74627
rmccaffr <robertmccaffrey@Roberts-MacBook-Pro-3.local>
2015-10-20 11:59:41 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,167 @@
+import StringIO
+import json
+import logging
+import random
+import urllib
+import urllib2
+from xml.dom import minidom
+
+# for sending images
+from PIL import Image
+import multipart
+
+# standard app engine imports
+from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+import webapp2
+
+TOKEN = '119152358:AAFvnvYU_5sxfTInk0LNQ55a_U5FMY3pyUo'
+
+BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+
+
+# ================================
+
+class EnableStatus(ndb.Model):
+    # key name: str(chat_id)
+    enabled = ndb.BooleanProperty(indexed=False, default=False)
+
+
+# ================================
+
+def setEnabled(chat_id, yes):
+    es = EnableStatus.get_or_insert(str(chat_id))
+    es.enabled = yes
+    es.put()
+
+def getEnabled(chat_id):
+    es = EnableStatus.get_by_id(str(chat_id))
+    if es:
+        return es.enabled
+    return False
+
+
+# ================================
+
+class MeHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getMe'))))
+
+
+class GetUpdatesHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getUpdates'))))
+
+
+class SetWebhookHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        url = self.request.get('url')
+        if url:
+            self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': url})))))
+
+
+class WebhookHandler(webapp2.RequestHandler):
+    def post(self):
+        urlfetch.set_default_fetch_deadline(60)
+        body = json.loads(self.request.body)
+        logging.info('request body:')
+        logging.info(body)
+        self.response.write(json.dumps(body))
+
+        update_id = body['update_id']
+        message = body['message']
+        message_id = message.get('message_id')
+        date = message.get('date')
+        text = message.get('text')
+        fr = message.get('from')
+        chat = message['chat']
+        chat_id = chat['id']
+
+        if not text:
+            logging.info('no text')
+            return
+
+        def reply(msg=None, img=None):
+            if msg:
+                resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
+                    'chat_id': str(chat_id),
+                    'text': msg.encode('utf-8'),
+                    'disable_web_page_preview': 'true',
+                    'reply_to_message_id': str(message_id),
+                })).read()
+            elif img:
+                resp = multipart.post_multipart(BASE_URL + 'sendPhoto', [
+                    ('chat_id', str(chat_id)),
+                    ('reply_to_message_id', str(message_id)),
+                ], [
+                    ('photo', 'image.jpg', img),
+                ])
+            else:
+                logging.error('no msg or img specified')
+                resp = None
+
+            logging.info('send response:')
+            logging.info(resp)
+
+        if text.startswith('/'):
+            if text == '/start':
+                reply('Bot enabled')
+                setEnabled(chat_id, True)
+            elif text == '/stop':
+                reply('Bot disabled')
+                setEnabled(chat_id, False)
+            elif text == '/image':
+                img = Image.new('RGB', (512, 512))
+                base = random.randint(0, 16777216)
+                pixels = [base+i*j for i in range(512) for j in range(512)]  # generate sample image
+                img.putdata(pixels)
+                output = StringIO.StringIO()
+                img.save(output, 'JPEG')
+                reply(img=output.getvalue())
+            elif text in ('/Pearse','/pearse')	:
+				input_url='http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=Dublin%20Pearse'
+				response = urllib2.urlopen(input_url)
+				xmldoc = minidom.parse(response)
+				root= xmldoc.getElementsByTagName('objStationData')
+				output=''
+				for child in root:
+					Destination=child.getElementsByTagName('Destination')
+					Duein=child.getElementsByTagName('Duein')
+					output+=Destination[0].firstChild.data+" due in " + Duein[0].firstChild.data + " mins\n"
+				if output=='' :
+					reply('Invalid station or no trains are currently available.')
+				else:
+					reply(output)
+
+            elif text == '/info':
+                reply(' Type / followed by your station name for live departure times.\nResults:"Destination" due in "X" mins.\nCreated by Robert McCaffrey')
+            else:
+				station=text[1:]
+				input_url='http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc='
+				input_url=input_url+station
+				response = urllib2.urlopen(input_url)
+				xmldoc = minidom.parse(response)
+				root= xmldoc.getElementsByTagName('objStationData')
+				output=''
+				for child in root:
+					Destination=child.getElementsByTagName('Destination')
+					Duein=child.getElementsByTagName('Duein')
+					output+=Destination[0].firstChild.data+" due in " + Duein[0].firstChild.data + " mins\n"
+				if output=='' :
+					reply('Invalid station or no trains are currently available.')
+				else:
+					reply(output)
+            
+
+       
+
+
+app = webapp2.WSGIApplication([
+    ('/me', MeHandler),
+    ('/updates', GetUpdatesHandler),
+    ('/set_webhook', SetWebhookHandler),
+    ('/webhook', WebhookHandler),
+], debug=True)
